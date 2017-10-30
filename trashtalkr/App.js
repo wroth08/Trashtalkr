e11610b580a17d9dfb394477583b34d00e70ff82
WESTON ROTH <weston.roth08@gmail.com>
2017-10-30 11:20:20 -0600

    basic chat box

--- trashtalkr/App.js
+++ trashtalkr/App.js
@@ -1,23 +1,51 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, Image } from 'react-native'
+
+import Chat from './components/Chat'
 
 export default class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      messages: [
+      {content: "You suck", time: "", sent_by: "Eddy"},
+      {content: "Try harder nerd", time: "", sent_by: "Eddy"},
+      {content: "Eat 💩", time: "", sent_by: "Eddy"},
+      {content: "My gran plays better than you", time: "", sent_by: "Eddy"},
+      {content: "Suck it loser", time: "", sent_by: "Eddy"},
+      {content: "Loooooooooooser", time: "", sent_by: "Eddy"},
+      {content: "lol", time: "", sent_by: "Eddy"},
+      {content: "The price good men pay for indifference to public affairs is to be ruled by evil men", time: "", sent_by: "Eddy"},
+      {content: "😂😂😂😂😂😂 😂😂😂😂😂 😂😂", time: "", sent_by: "Eddy"},
+      {content: "💩💩💩💩💩💩💩💩💩💩💩", time: "", sent_by: "Eddy"},
+      {content: "Maybe you should just give up", time: "", sent_by: "Eddy"},
+      {content: "Your team is trash", time: "", sent_by: "Eddy"},
+      {content: "Are you even trying?", time: "", sent_by: "Eddy"},
+      {content: "Just quit already", time: "", sent_by: "Eddy"},
+      {content: "Wow you suck", time: "", sent_by: "Eddy"},
+    ]
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <View style={styles.pad}></View>
+        <Chat messages={this.state.messages}/>
+        <View style={styles.pad}></View>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'aqua'
   },
-});
+  pad: {
+    height: 50
+  }
+})
---
+++ trashtalkr/components/Chat.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { StyleSheet, Text, FlatList, Image } from 'react-native'
+
+import Message from './Message'
+
+class Chat extends React.Component {
+    render () {
+        return (
+            <FlatList
+            data={this.props.messages}
+            renderItem={({item}) => <Message key={item.content} message={item}></Message>}
+          />
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    contentContainer: {
+        flex: 1,
+        backgroundColor: 'aqua',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-end',
+        width: '100%',
+        padding: 20,
+        marginTop: 50,
+    }
+  })
+
+export default Chat
